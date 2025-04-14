e3225336dbd5e3711e48ff0f004d2329d0d843e1
jmangum <jmangum@nrao.edu>
2025-04-14 17:45:41 +0000

    Deploying to gh-pages from @ jmangum/jmangum.github.io@4f7af9222b283ddbb726c55704e2f3bcbe0941b1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,7 +28,7 @@ ninja.data = [{
           description: "Curriculum Vitae for Jeff Mangum.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/https:/jmangum.github.io/cv/Mangum_cV_20250408.pdf";
+            window.location.href = "/https:/jmangum.github.io/cv/Mangum_CV_20250408.pdf";
           },
         },{id: "nav-people",
           title: "people",
