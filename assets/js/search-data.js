b3cd08ccb3f4881d0742a6a201f39eb4d8176293
jmangum <jmangum@nrao.edu>
2025-04-29 19:00:02 +0000

    Deploying to gh-pages from @ jmangum/jmangum.github.io@bc796db07806e2232d9f2011922bc05d0d7d8ad8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -64,11 +64,6 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
@@ -109,6 +104,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-project-1",
+          title: 'project 1',
+          description: "Molecular Spectral Line and Millimeter Continuum Emission",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/sfgalaxies/";
             },},{
         id: 'social-bluesky',
         title: 'Bluesky',
