00db8a8cf10896b518ff277eed778823d16a41be
jmangum <jmangum@nrao.edu>
2025-06-04 15:54:21 +0000

    Deploying to gh-pages from @ jmangum/jmangum.github.io@f1d120277b83828e9162a6222594d698313f297a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -84,11 +84,6 @@ ninja.data = [{
           description: "a project with no image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
             },},{id: "projects-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
@@ -104,6 +99,11 @@ ninja.data = [{
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/moved/2_project/";
+            },},{id: "projects-radio-telescopes",
+          title: 'Radio Telescopes',
+          description: "Design, Construction, and Performance of Radio Telescopes",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/radiotelescopes/";
             },},{id: "projects-star-formation-in-galaxies",
           title: 'Star Formation in Galaxies',
           description: "",
