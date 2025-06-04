c045317a35fb586b542fad2d06955cbe4e3dc424
jmangum <jmangum@nrao.edu>
2025-06-04 15:41:13 +0000

    Deploying to gh-pages from @ jmangum/jmangum.github.io@a990a1385971de93e3ef8af304d62c59f435d47f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -64,11 +64,6 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
@@ -104,6 +99,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-project-2",
+          title: 'project 2',
+          description: "a project with a background image and giscus comments",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/moved/2_project/";
             },},{id: "projects-star-formation-in-galaxies",
           title: 'Star Formation in Galaxies',
           description: "",
