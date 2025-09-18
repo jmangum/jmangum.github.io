2d403db4d042069c156da077ed7d1ba9abcc1fbc
jmangum <jmangum@nrao.edu>
2025-09-18 23:16:00 +0000

    Deploying to gh-pages from @ jmangum/jmangum.github.io@0e7a73eeefa4edc14a6cf899e393742d52449bdd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -71,6 +71,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
+            },},{id: "fun-bedside-clock",
+          title: 'Bedside Clock',
+          description: "Bedside clock that shows when the Sun is up and Moon phases",
+          section: "Fun",handler: () => {
+              window.location.href = "/fun/BedsideClock/";
             },},{id: "fun-iot-bird-animal-feeder",
           title: 'IoT Bird (Animal) Feeder',
           description: "A motion-activated camera catches hungry animals that visit this IoT feeder",
@@ -81,6 +86,11 @@ ninja.data = [{
           description: "A very cool pulsing LED floor lamp",
           section: "Fun",handler: () => {
               window.location.href = "/fun/LEDFloorLamp/";
+            },},{id: "fun-weather-station",
+          title: 'Weather Station',
+          description: "My first electronics project, and still my favorite",
+          section: "Fun",handler: () => {
+              window.location.href = "/fun/WeatherStation/";
             },},{id: "projects-pasp",
           title: 'PASP',
           description: "Publications of the Astronomical Society of the Pacific",
